d6d327a7c69457d47b6ea10a996cd10fa260d0cb
gitsetgambit <adityakumarvxp@gmail.com>
2022-05-17 16:54:15 +0530

    glide user methods

---
+++ GlideForm & GlideUser/2_GlideUser_Methods.js
@@ -0,0 +1,17 @@
+//todo: firstName & lastName
+alert(g_user.firstName + '  ' + g_user.lastName); //this gets the first and lastname of the current user 
+
+//todo: getFullName()
+alert('hello' + g_user.getFullName()); //gets the full name and its a method cuz it needs "()" to run
+
+//todo: hasRoles()
+alert('do u have any roles' + g_user.hasRoles()); //it asks if it has any roles no matter what roles 
+
+//todo: hasRole()
+alert('do u have any ITIL role' + g_user.hasRole()); //it checks for the ITIL role 
+
+// hasRoles and hasRole are diff one check for any role, another checks
+// for only the ITIL roles 🤣 no just the diff of 's' 
+
+//todo: userName()
+alert('your username is' + g_user.userName); //gets the username
