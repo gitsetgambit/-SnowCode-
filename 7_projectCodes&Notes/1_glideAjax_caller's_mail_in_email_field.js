1e0b7ed1d0693e3b661c1fcf4dddaea47ad29e41
gitsetgambit <adityakumarvxp@gmail.com>
2022-06-02 18:10:42 +0530

    a

---
+++ 7_projectCodes&Notes/1_glideAjax_caller's_mail_in_email_field.js
@@ -0,0 +1,43 @@
+// TASK 
+// make a email field in the incident form and whenever the caller is selected then his email should come in the email field 
+
+//  APPROACH
+//-  make a string field name email
+//- make a script include
+//- make a client script 
+
+//todo: 
+//CLIENT SCRIPT💻
+function onChange(control, oldValue, newValue, isLoading, isTemplate) { //preloaded code
+    if (isLoading || newValue === '') {
+        return;                                                         
+    }                                                                   //till here preloaded code
+    var GA = new GlideAjax('TASKcallersEmailOnTheFieldSI');             //calling glideajax 
+    GA.addParam('sysparm_name', 'emailValue');                          //giving function name sysparm_name is fix syntax for asking function name 
+    GA.addParam('callersEmail', g_form.getValue('caller_id'));          //here it is using g_form.getValue for getting the caller_id where caller is basically sys id in this case 
+    GA.getXML(ajaxProcessor);                                           //we're using getXML to fetch the data coming from script include in the for of XML 
+    function ajaxProcessor(response) {                                  //now we're processing the XML inside this function 
+        var a = response.responseXML.documentElement.getAttribute("answer"); //from walking we're going inside the XML to the "answer" property 
+        g_form.setValue('u_email', a);                                  //setting the value of email field 
+    }
+
+}
+
+//SCRIPT INCLUDE👨‍💻
+var TASKcallersEmailOnTheFieldSI = Class.create();                                          //preloaded code
+TASKcallersEmailOnTheFieldSI.prototype = Object.extendsObject(AbstractAjaxProcessor, {      //till here preloaded code
+                                                                                            
+    emailValue: function() {                                                                //we created a property and function as the value of the property             
+        var parameterReceived = this.getParameter('callersEmail');                          //we're getting the sys_id sent to us from g_form line 
+    var incidentGR = new GlideRecord('sys_user');                                           //simple gliderecord creation
+        incidentGR.addQuery('sys_id', parameterReceived);                                   //it'll ask to run query with sys_id so enter the varible carrying sys_id
+        incidentGR.query();                                                                 //run query
+        if (incidentGR.next()) {                                                            //it'll ask if the record exist         
+            var e = incidentGR.email;
+            return e;                                                                       //returning the email it can be out of the if {} common coding logics will apply
+        }
+
+    },
+
+    type: 'TASKcallersEmailOnTheFieldSI'
+});
